c3d83cab8e975fc84c73e6fe3bc97e8605670c1a
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-18 00:22:43 +0200

    added styling to login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useAuthState } from "react-firebase-hooks/auth"
 import { auth } from '../firebase';
+import Login from "./login"
 
 
 function MyApp({ Component, pageProps }: AppProps) {
