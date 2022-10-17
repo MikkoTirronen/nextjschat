182fbb3ef3fd84c04bd828ae7f3c39305ff6b95e
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-17 23:11:58 +0200

    navigate to login when no user detected

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from '../firebase';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [user] = useAuthState(auth as any);
+
+  if(!user) return <Login/>
   return <Component {...pageProps} />
 }
 
