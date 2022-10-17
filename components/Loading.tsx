1f505892617e308e6ec8cdff1fbb907377517ebe
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-18 00:54:56 +0200

    created loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,19 @@
+import { autocompleteClasses } from '@mui/material';
+import React from 'react'
+
+function Loading() {
+  return (
+    
+      <div style= {{textAlign: 'center' }}>
+        <img
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+          alt="loading"
+          style={{ marginBottom: 10 }}
+          height={200}
+        />
+      </div>
+    
+  );
+}
+
+export default Loading
