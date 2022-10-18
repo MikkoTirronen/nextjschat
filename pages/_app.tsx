c55fa4551bbbc9f056040e16d990bfadd3f8b40d
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-18 19:04:51 +0200

    added useEffect to get userData

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,34 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useAuthState } from "react-firebase-hooks/auth"
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
 import Login from "./login"
 import Loading from '../components/Loading';
-
+import firebase from "firebase/compat/app";
+import {useEffect} from "react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [user, loading] = useAuthState(auth as any);
 
-  if(true) return <Loading/>
+  interface CurrentUser {
+    email: string;
+    lastSeen?: Date;
+    photoUrl?: string;
+  }
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users").doc(user.uid).set({
+        email: user.email as String,
+        photoUrl: user.photoURL as String,
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+      },{merge:true})
+    }
+  
+  }, [user]);
+  
+
+  if(loading) return <Loading/>
   if(!user) return <Login/>
   return <Component {...pageProps} />
 }
