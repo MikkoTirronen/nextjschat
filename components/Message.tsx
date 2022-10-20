0f7fab742f3f8f47b1db4d1f8e82a5f7c1061f06
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-21 01:28:35 +0200

    added initial message component

---
+++ components/Message.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Message() {
+  return (
+    <Container>Message</Container>
+  )
+}
+
+export default Message
+
+const Container = styled.div``
