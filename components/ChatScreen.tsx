98a65e968b92ecfd741936efb3db8148fd834243
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-23 23:38:32 +0200

    added recipient avatar and lastseen functionality

--- components/ChatScreen.tsx
+++ components/ChatScreen.tsx
@@ -2,83 +2,121 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
 import { auth, db } from "../firebase";
 import { useRouter } from "next/router";
-import { Avatar } from "@mui/material";
+import { Avatar, IconButton } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import { useCollection } from "react-firebase-hooks/firestore";
-import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 import MicIcon from "@mui/icons-material/Mic";
 import Message from "./Message";
-import firebase  from "firebase/compat/app";
+import firebase from "firebase/compat/app";
 import { Firestore, Timestamp } from "firebase/firestore";
 import React from "react";
-
+import getRecipientEmail from "../utils/getRecipientEmail";
+import TimeAgo from "timeago-react"
 export interface InputProps {
-    input?: string;
-    setInput: (inputs: string) => void;
-};
-interface Data{
-  chat: string[];
-  messages: string[];
+  input?: string;
+  setInput: (inputs: string) => void;
+}
+export interface Data {
+  chat: { users: { [key: number]: number; email: string } };
+  messages: string;
 }
 
-export interface currentUser{
+export interface currentUser {
   email: string;
   lastSeen: Timestamp;
   photoUrl: string;
 }
 
-export function ChatScreen({ chat, messages }: any) {
- 
+export function ChatScreen({ chat, messages }: Data) {
   const [user] = useAuthState(auth as any);
   const [input, setInput] = React.useState("");
-  
+
   const router = useRouter();
-  const messagesByIdList = db
-      .collection("chat")
+
+  const [messagesSnapshot] = useCollection(
+    db
+      .collection("chats")
       .doc(router.query.id as any)
       .collection("messages")
-      .orderBy("timestamp", "asc")
-  const [messagesSnapshot] = useCollection(messagesByIdList as any);
+      .orderBy("timestamp", "asc") as any
+  );
+
+  const [recipientSnapshot] = useCollection(
+    db
+      .collection("users")
+      .where("email", "==", getRecipientEmail(chat.users, user)) as any
+  );
+
+  const recipient = recipientSnapshot?.docs?.[0].data();
+  const recipientEmail = getRecipientEmail(chat.users, user)
 
   const showMessages = () => {
     if (messagesSnapshot) {
-      return messagesSnapshot.docs.map(message => (
-        <Message key={message.id} user={message.data().user} message={{ ...message.data(), timestamp: message.data().timestamp?.toDate().getTime(), }} />
-      ))
+     
+      return messagesSnapshot.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
     } else {
-      return JSON.parse(messages).map((message: { id: React.Key; user: currentUser; }) => (
-        <Message key={message.id} user={message.user} message={message} />
-      ))
-    
-    };
-  }
-  const sendMessage = (e: { preventDefault: () => void; }) => {
+      return JSON.parse(messages).map(
+        (message: { id: React.Key; user: currentUser }) => (
+          <Message key={message.id} user={message.user} message={message} />
+        )
+      );
+    }
+  };
+  const sendMessage = (e: { preventDefault: () => void }) => {
     e.preventDefault();
 
     //update lastSeen timestamp
-    db.collection("users").doc(user?.uid as string).set({
-      lastSeen: firebase.firestore.FieldValue.serverTimestamp()
-    }, { merge: true });
+    db.collection("users")
+      .doc(user?.uid as string)
+      .set(
+        {
+          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+        },
+        { merge: true }
+      );
 
     //add message to collection "messages" in database
-    db.collection("chats").doc(router.query.id as any).collection("messages").add({
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      message: input,
-      user: user?.email,
-      photoUrl: user?.photoURL
-    })
+    db.collection("chats")
+      .doc(router.query.id as any)
+      .collection("messages")
+      .add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        message: input,
+        user: user?.email,
+        photoUrl: user?.photoURL,
+      });
 
     setInput("");
-  }
+  };
 
   return (
     <Container>
       <Header>
-        <Avatar />
+        {recipient && recipient? (<Avatar src={recipient?.photoUrl} />) : (<Avatar >{recipientEmail[0]}</Avatar>)}
+        
+
         <HeaderInformation>
-          <h3>Rec Email</h3>
-          <p> lastSeen</p>
+          <h3>{recipientEmail}</h3>
+          {recipientSnapshot ? (
+            <p>
+              last active: {" "}
+              {recipient?.lastSeen?.toDate() ? (
+                <TimeAgo datetime={recipient?.lastSeen?.toDate()} />
+              ) : (
+                "unavailable"
+              )}</p>
+              ): "loading"}
         </HeaderInformation>
         <HeaderIcons>
           <IconButton>
@@ -89,18 +127,20 @@ export function ChatScreen({ chat, messages }: any) {
           </IconButton>
         </HeaderIcons>
       </Header>
-      <MessageContainer>{showMessages()}<EndOfMessage /></MessageContainer>
+      <MessageContainer>
+        {showMessages()}
+        <EndOfMessage />
+      </MessageContainer>
       <InputContainer>
         <InsertEmoticonIcon />
-        <Input value={input} onChange={e => setInput(e.target.value)} />
+        <Input value={input} onChange={(e) => setInput(e.target.value)} />
         <button hidden disabled={!input} type="submit" onClick={sendMessage} />
-        <MicIcon/>
+        <MicIcon />
       </InputContainer>
-      
     </Container>
   );
 }
-  export default ChatScreen;
+export default ChatScreen;
 
 const Container = styled.div``;
 
@@ -129,10 +169,9 @@ const HeaderInformation = styled.div`
   }
 `;
 const HeaderIcons = styled.div``;
-const IconButton = styled.div``;
 const MessageContainer = styled.div`
   padding: 30px;
-  background-color:#e5ded8;
+  background-color: #e5ded8;
   min-height: 90vh;
 `;
 const EndOfMessage = styled.div``;
@@ -144,13 +183,14 @@ const InputContainer = styled.form`
   position: sticky;
   background-color: white;
   z-index: 100;
+  bottom: 0;
 `;
 
 const Input = styled.input`
   flex: 1;
   align-items: center;
   padding: 20px;
-  position:sticky;
+  position: sticky;
   bottom: 0;
   background-color: whitesmoke;
   outline: 0;
